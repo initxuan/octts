bb169d17c9da2807d8fe211dbcb7b83d09fd21c0
zhangtao <zhangtao103239@163.com>
2017-06-27 09:37:26 +0800

    Initial commit

---
+++ src/main/java/com/hyzzcz/OcttsApplication.java
@@ -0,0 +1,12 @@
+package com.hyzzcz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcttsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcttsApplication.class, args);
+	}
+}
